69acb26886aecadcb54dfbcb6fb29ecd88a7ad0b
donglin <jonathan.dong.lin@gmail.com>
2014-01-04 16:17:33 +0800

    初始代码

---
+++ dolphin/js/Ball.js
@@ -0,0 +1,130 @@
+﻿
+(function(){
+
+var Ball = game.Ball = function(props)
+{
+	props = props || {};
+	this.type = props.type;
+	Ball.superClass.constructor.call(this, this.type);
+	this.id = props.id || Q.UIDUtil.createUID("Ball");
+	
+	this.reset(this.type);
+};
+Q.inherit(Ball, Q.MovieClip);
+
+Ball.prototype.init = function()
+{
+	
+};
+
+Ball.prototype.update = function(timeInfo)
+{
+	this.rotation += 0.5;
+};
+
+Ball.prototype.reset = function(type)
+{
+	this.setType(type);
+	this.currentScore = this.type.score;
+	this.alpha = 1;
+	this.fading = false;
+	this.bouncing = false;
+	this.currentSpeedY = this.speedY;
+	this.currentSpeedX = 0;
+	this.delay = Math.floor(Math.random()*50);
+	
+	this.setRandomPosition();
+}
+
+Ball.prototype.setRandomPosition = function()
+{
+	var minX = 100, maxX = game.width-100, minY = -100, maxY = 0;
+	this.x = Math.floor(Math.random()*(maxX-minX)+minX);
+	//this.y = Math.floor(Math.random()*(maxY-minY)+minY);
+	this.y = -50;
+}
+
+Ball.prototype.setType = function(type)
+{
+	this.type = type;
+	this._frames.length = 0;
+	this.addFrame(type.frames);
+	this.currentFrame = 0;
+}
+
+Ball.getRandomType = function()
+{
+	var list = this.TypeList;
+	var r = Math.floor(Math.random()*list.length);
+	return list[r];
+};
+
+Ball.prototype.getCollide = function()
+{
+	this.currentScore += this.type.scoreStep;
+	if(this.currentScore > this.type.maxScore) this.currentScore = this.type.maxScore;
+	this.currentSpeedY = -10;
+	this.bouncing = true;
+}
+
+Ball.prototype.stopBounce = function()
+{
+	//this.currentSpeedY = this.speedY;
+	this.bouncing = false;
+}
+
+Ball.init = function() 
+{
+	this.Type = {};
+	this.Type.small = 
+	{
+		image:game.getImage("ball"),
+		regX: 94,
+		regY: 92,
+		width: 188,
+		height: 184,
+		score: 0,
+		scoreStep: 1,
+		maxScore: 3,
+		speedY: 0,
+		frames:[
+			{rect:[0,0,188,184]}
+			]
+	};
+	
+	this.Type.medium =
+	{
+		image:game.getImage("ball"),
+		regX: 94,
+		regY: 92,
+		width: 188,
+		height: 184,
+		score: 1,
+		scoreStep: 1,
+		maxScore: 5,
+		speedY: 0.5,
+		frames:[
+			{rect:[188,0,188,184]}
+			]
+	};
+
+	this.Type.big = 
+	{
+		image:game.getImage("ball"),
+		regX: 94,
+		regY: 92,
+		width: 188,
+		height: 184,
+		score: 2,
+		scoreStep: 1,
+		maxScore: 8,
+		speedY: 1,
+		frames:[
+			{rect:[0,184,188,184]}
+			]
+	};
+
+	this.TypeList = [this.Type.small, this.Type.medium, this.Type.big];
+};
+
+})();
---
+++ dolphin/js/Dolphin.js
@@ -0,0 +1,74 @@
+﻿
+(function(){
+
+var Dolphin = game.Dolphin = function(props)
+{
+	props = props || {};
+	Dolphin.superClass.constructor.call(this, props);
+	this.id = props.id || Q.UIDUtil.createUID("Dolphin");
+	
+	this.avatar = null;	
+	this.jumping = false;
+	this.moving = false;
+	
+	this.init();
+}
+Q.inherit(Dolphin, Q.DisplayObjectContainer);
+
+Dolphin.prototype.init = function()
+{
+	var avatar = new Q.MovieClip({id:"dolphin", image:game.getImage("dolphin"), interval:120});
+	avatar.addFrame([
+	{rect:[0,0,223,337], label:"idle"},
+	{rect:[223,0,223,337]},
+	{rect:[0,337,223,337]},	
+	{rect:[223,0,223,337], jump:"idle"},
+	{rect:[223,337,189,329], label:"jump"},
+	]);
+	
+	this.width = 223;
+	this.height = 337;
+	this.currentSpeedX = this.speedX = 5;
+	this.currentSpeedY = this.speedY = 10;
+	this.dirX = 0;
+	this.dirY = 0;
+	this.oldY = 0;
+	
+	this.avatar = avatar;
+	this.addChild(avatar);
+};
+
+Dolphin.prototype.move = function(dir)
+{
+	if(this.moving) return;
+	this.dirX = dir;
+	this.currentSpeedX = this.speedX;
+	this.moving = true;
+}
+
+Dolphin.prototype.stopMove = function()
+{
+	this.dirX = 0;
+	this.currentSpeedX = this.speedX;
+	this.moving = false;
+}
+
+Dolphin.prototype.jump = function()
+{
+	if(this.jumping) return;
+	this.oldY = this.y;
+	this.dirY = 1;
+	this.currentSpeedY = this.speedY;
+	this.jumping = true;
+	this.avatar.gotoAndStop("jump");
+}
+
+Dolphin.prototype.stopJump = function()
+{
+	this.y = this.oldY;
+	this.dirY = 0;
+	this.jumping = false;
+	this.avatar.gotoAndPlay("idle");
+}
+
+})();
